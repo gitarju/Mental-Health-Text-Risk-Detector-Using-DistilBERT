e9bb8e40b95f304edf4b390e4eea151aff17bd59
gitarju <arjunajith.work@gmail.com>
2026-05-13 11:11:03 +0530

    Clean up repository for production deployment

--- fix_notebook.py
+++
@@ -1,6 +0,0 @@
-with open('model_training.ipynb', 'r') as f:
-    content = f.read()
-content = content.replace('"    tokenizer=tokenizer,\\n",', '"    processing_class=tokenizer,\\n",')
-with open('model_training.ipynb', 'w') as f:
-    f.write(content)
-print("Fixed notebook.")
